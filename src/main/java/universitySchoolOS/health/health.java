6dff3ae4889f2427109e688e8248aed75395c895
lahumisal <lahumisal402@gmail.com>
2026-08-13 18:59:55 +0530

    rename package name

--- src/main/java/universitySchoolOS/health/health.java
+++ src/main/java/universitySchoolOS/health/health.java
@@ -1,4 +1,4 @@
-package UniversitySchoolOS.health;
+package universitySchoolOS.health;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
