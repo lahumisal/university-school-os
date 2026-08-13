d244c9d96c0b87ac0a8366b6045344b17e4eda7a
lahumisal <lahumisal402@gmail.com>
2026-08-14 00:11:58 +0530

    added hikaricp connection and slf4j logs

---
+++ src/main/java/universitySchoolOS/Database/DatabaseConfig.java
@@ -0,0 +1,37 @@
+package universitySchoolOS.Database;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import lombok.extern.slf4j.Slf4j;
+import javax.sql.DataSource;
+
+@Configuration
+@Slf4j
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Bean
+    public DataSource dataSource() {
+        log.info("loading datasource configuration...");
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName(driverClassName);
+        log.info("datasource loaded successfully.");
+        return dataSource;
+    }
+}
