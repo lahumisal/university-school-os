da7eabd1656b0a8a56a7a2857ee430efadc51ba0
lahumisal <lahumisal402@gmail.com>
2026-08-22 23:26:04 +0530

    added logs inside cors config

--- src/main/java/universitySchoolOS/cors/CorsConfig.java
+++ src/main/java/universitySchoolOS/cors/CorsConfig.java
@@ -1,5 +1,6 @@
 package universitySchoolOS.cors;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -7,6 +8,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 
+@Slf4j
 @Configuration
 public class CorsConfig {
 
@@ -20,17 +22,17 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
-
+        log.info("configuration initialized..");
         configuration.setAllowedOrigins(Arrays.asList(corsProperties.getAllowedOrigins().split(",")));
         configuration.setAllowedMethods(Arrays.asList(corsProperties.getAllowedMethods().split(",")));
         configuration.setAllowedHeaders(Arrays.asList(corsProperties.getAllowedHeaders().split(",")));
         configuration.setExposedHeaders(Arrays.asList(corsProperties.getExposedHeaders().split(",")));
         configuration.setAllowCredentials(corsProperties.isAllowCredentials());
         configuration.setMaxAge(corsProperties.getMaxAge());
-
+        log.info("loaded configuration properties successfully");
         UrlBasedCorsConfigurationSource source =new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
-
+        log.info("added source.");
         return source;
     }
 }
